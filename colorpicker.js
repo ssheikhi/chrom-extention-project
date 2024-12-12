6f25e483366240ad09f519e97e908f89bdd76acb
ssheikhi <fx.sheikhi@gmail.com>
2024-12-12 03:15:44 +0100

    remove buttun

--- colorpicker.js
+++ colorpicker.js
@@ -1,4 +1,4 @@
-document.getElementById('copyColor').addEventListener('click', () => {
+document.getElementById('colorPicker').addEventListener('input', () => {
   const colorPicker = document.getElementById('colorPicker');
   navigator.clipboard.writeText(colorPicker.value);
   alert('Color copied to clipboard!');
